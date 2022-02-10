2196b98ad100be9cb6776d564ffe598dd7489ed0
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-02-10 16:45:41 +0200

    Add public dir

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes());
 app.use(shopRoutes());
